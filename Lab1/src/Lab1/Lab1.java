edd56fbb584d929094f356a98935cefd0ce572e6
ThadakornDev01 <Thadakon2004@gmail.com>
2025-11-20 17:21:39 +0700

    Add files via upload

--- Lab1/src/Lab1/Lab1.java
+++ Lab1/src/Lab1/Lab1.java
@@ -5,15 +5,17 @@ public class Lab1 {
 	public static void main(String[] args) {
 		Scanner score = new Scanner(System.in);
 		
-
+		System.out.print("กรอกตัวเลขตัวที่ 1. ");
 		int num1 = score.nextInt();
 		
-
+		System.out.print("กรอกตัวเลขตัวที่ 2. ");
 		int num2 = score.nextInt();
 		
 		int sum = num1 + num2;
 		
-		System.out.print(sum);
+		System.out.print("รวมผลบวก = " +sum);
 	}
 
 }
+
+
