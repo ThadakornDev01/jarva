3b61dd92a383c236079b35c1b5588c1de1d7d8c3
ThadakornDev01 <Thadakon2004@gmail.com>
2025-11-22 00:34:33 +0700

    Add files via upload

---
+++ Lab1/src/Lab1/Lab1_7.java
@@ -0,0 +1,19 @@
+package Lab1;
+import java.util.Scanner;
+public class Lab1_7 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		
+		int sum = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			int num = input.nextInt();
+			sum = sum + num;
+		}
+		
+		System.out.print(sum);
+	}
+
+}
