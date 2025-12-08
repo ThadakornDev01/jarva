acbaf2f348d61f0530726b8c5c58b05c651b1014
ThadakornDev01 <Thadakon2004@gmail.com>
2025-12-08 21:10:21 +0700

    Add files via upload

---
+++ Lab3/src/Lab3/Lab3_1.java
@@ -0,0 +1,26 @@
+package Lab3;
+import java.util.Scanner;
+
+class User {
+	private String username;
+	
+	User(String username){
+		this.username = username;
+	}
+	public String getUsername(){
+		return this.username;
+	}
+}
+
+public class Lab3_1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String inputName = scanner.nextLine();
+		User user = new User(inputName); //ส่ง inputName เข้าไปใน Constructor ของ User
+		
+		System.out.print(user.getUsername()); //แสดงผลชื่อผู้ใช้ โดยการเรียกใช้เมธอด getUsername()
+		scanner.close();
+	}
+
+}
---
+++ Lab3/src/Lab3/Lab3_2.java
@@ -0,0 +1,35 @@
+package Lab3;
+import java.util.Scanner;
+
+class Player{
+	private int score;
+	
+	Player(int score){ //Constructor รับค่าเริ่มต้น
+		this.score = score;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	public void setScore(int newScore) {
+		this.score = newScore;
+	}
+	
+}
+
+public class Lab3_2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int initialScore = scanner.nextInt();
+		int updatedScore = scanner.nextInt();
+		
+		Player Py = new Player(initialScore);
+		Py.setScore(updatedScore); //เรียกใช้เมธอด setScore() เพื่ออัปเดตคะแนน
+		System.out.println(Py.getScore());
+		
+		scanner.close();
+	}
+
+}
